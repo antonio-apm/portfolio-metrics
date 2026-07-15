a0c6a991481e18dfe8a80c8d4c5f39ac98f5df0d
antonio_melacini <152217836+antonio-apm@users.noreply.github.com>
2026-07-14 23:32:29 -0400

    layout

--- webapp.py
+++ webapp.py
@@ -130,7 +130,7 @@ def main():
 
     corr = portfolio.dependence(type="corr", tail=tail)
 
-    st.markdown("**Portfolio Data:** *(choose inpnuts in sidebar)*")
+    st.markdown("**Portfolio Data:** \t\t *(choose inpnuts in sidebar)*")
 
     col1, col2, col3 = st.columns(3)
     col1.text("Securities:" + ", ".join(tickers))
